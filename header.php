699dfce2bb560ce35fdfe4231738401859310214
YUSUKE-5995 <y.watanabe20120725@gmail.com>
2021-08-15 17:30:40 +0900

    Create header.php
    What you at least need to describe when you make a header file of HTML

---
+++ header.php
@@ -0,0 +1,44 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+    <title>そのページのタイトル</title>
+    <meta name="description" content="ページの内容を表す文章" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta name="googlebot" content="index,follow"><!-- Google用 -->
+    <!-- SNS対応 -->
+    <meta property="og:url" content="ページのURL" />
+    <meta property="og:title" content="ページのタイトル" />
+    <meta property="og:type" content="ページのタイプ">
+    <meta property="og:description" content="記事の抜粋" />
+    <meta property="og:image" content="画像のURL" />
+    <meta name="twitter:card" content="カード種類" />
+    <meta name="twitter:site" content="@Twitterユーザー名" />
+    <meta property="og:site_name" content="サイト名" />
+    <meta property="og:locale" content="ja_JP" />
+    <meta property="fb:app_id" content="appIDを入力" />
+    <!-- 電話番号やメールアドレスの変換設定 -->
+    <meta name="format-detection" content="email=no,telephone=no,address=no" />
+    <!-- クライアント識別のためにブラウザにCookieを保存します -->
+    <meta http-equiv="set-cookie" content="name=value; expires=date; path=url">
+    <!-- 著者名の明記 -->
+    <meta name=”author” content=”著者名”>
+    <!--URLの正規化  -->
+    <link rel="canonical" href="正規化するURL" />
+    <!-- サイトアイコン -->
+    <link rel="icon" href="画像URL" sizes="16x16" type="image/png" />
+    <link rel="icon" href="画像URL" sizes="32x32" type="image/png" />
+    <link rel="icon" href="画像URL" sizes="48x48" type="image/png" />
+    <link rel="icon" href="画像URL" sizes="62x62" type="image/png" />
+    <!-- スマホ用アイコン画像 -->
+    <link rel="apple-touch-icon-precomposed" href="画像のURL" />
+    <!-- Windows用タイル設定 -->
+    <meta name="msapplication-TileImage" content="画像のURL" />
+    <meta name="msapplication-TileColor" content="カラーコード（例：#F89174）" />
+    <!-- フィードページのURLを指定 -->
+    <link rel="alternate" type="application/rss+xml" title="フィード" href="フィードページのURL" />
+    <!-- 分割ページ用のタグ -->
+    <link rel="prev" href="前のページのURL" />
+    <link rel="next" href="次のページのURL" />
+
+</head>
